1cb9f89adafb80184e711699625cb2d2f33a5a8f
Vajira <vajirawke@gmail.com>
2018-07-21 20:34:20 +0530

    add

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 var logger = require('./logger');
 
-mongoose.connect('mongodb://192.168.1.2:27017/oneall',{ useNewUrlParser: true });
+mongoose.connect('mongodb://192.168.1.2:27017/ranpatha',{ useNewUrlParser: true });
 
 mongoose.set("debug", (collectionName, method, query, doc) => {
     //logger.log(`${collectionName}.${method}`, JSON.stringify(query), doc);
